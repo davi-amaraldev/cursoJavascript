bf7b3d1b8af295096538fb0269b4d898d5ab9e1f
Davi Amaral <imyuno2022@outlook.com>
2025-12-27 16:23:37 -0300

    Subindo mais alguns conteúdos

---
+++ aula12/aula12.js
@@ -0,0 +1,9 @@
+let varA = 'A';
+let varB = 'B';
+let varC = 'C';
+
+console.log(varA, varB, varC);
+
+[varA, varB, varC] = [varC, varB, varA];
+
+console.log(varA, varB, varC);
---
+++ aula13/Strings.js
@@ -0,0 +1,6 @@
+// Strings são indexadas.
+// Cada caractere tem um índice, começando em 01234567...
+
+let umaString = "Um texto";
+
+console.log(umaString.match(/[a-z]/g))
