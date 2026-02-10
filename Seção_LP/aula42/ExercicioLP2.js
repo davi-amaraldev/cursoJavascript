b4294fb27d2838f74286eb43ebe93b5445d94ea0
Davi Amaral <imyuno2022@outlook.com>
2026-02-10 14:02:18 -0300

    Exercícios Feitos

---
+++ Seção_LP/aula42/ExercicioLP2.js
@@ -0,0 +1,10 @@
+let imageHeight, imageWidth;
+
+imageHeight = 50;
+imageWidth = 10;
+
+const ePaisagem = (height, width) => {
+    return height > width;
+}
+
+console.log(ePaisagem(imageHeight, imageWidth));
---
+++ Seção_LP/aula43/ExercicioLP3.js
@@ -0,0 +1,27 @@
+let num = 6;
+
+function verificarNumero(numero){
+    numero = num;
+    let resultado = '';
+    let verificaNaN = '';
+
+    if (numero % 3 == 0 && numero % 5 == 0){
+        resultado = 'FizzBuzz';
+    } else if (numero % 3 == 0){
+        resultado = 'Fizz';
+    } else if(numero % 5 == 0){
+        resultado = 'Buzz';
+    } else {
+        resultado = num;
+    }
+
+    if (isNaN(num)){
+        verificaNaN = true;;
+    } else{
+        verificaNaN = false;
+    }
+
+    return `${resultado} || É um NaN? ${verificaNaN}`
+}
+
+console.log(verificarNumero(num));
