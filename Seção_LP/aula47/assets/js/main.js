04625f5db0e8b7ffedb2ed3b926367534495893b
Davi Amaral <imyuno2022@outlook.com>
2026-02-15 12:45:01 -0300

    Aula 46 e Exercício

---
+++ Seção_LP/aula47/assets/js/main.js
@@ -0,0 +1,8 @@
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+
+iniciar.addEventListener('click', (event) => {
+
+})
