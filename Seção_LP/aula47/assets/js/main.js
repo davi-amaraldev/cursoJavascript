23db4deda767a3f1d26af5c69a36413842dc2ce9
Davi Amaral <imyuno2022@outlook.com>
2026-02-16 02:36:39 -0300

    Exercício Timer

--- Seção_LP/aula47/assets/js/main.js
+++ Seção_LP/aula47/assets/js/main.js
@@ -1,8 +1,62 @@
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
+function getHourOfSeconds(segundos){
+    const data = new Date(segundos * 1000);
 
-iniciar.addEventListener('click', (event) => {
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'GMT'
+    })
+}
 
+console.log(getHourOfSeconds(10))
+
+const clock = document.querySelector('.relogio');
+const init = document.querySelector('.iniciar');
+const pause = document.querySelector('.pausar');
+const zero = document.querySelector('.zerar');
+
+let seconds = 0;    
+let timer = null;
+
+function clockInit(){
+
+    if (timer !== null) return; 
+
+    timer = setInterval(() => {
+        seconds++;
+        clock.innerHTML = getHourOfSeconds(seconds);
+    }, 1000)
+
+}
+
+function setRunningStyle(){
+    clock.classList.remove('pausado', 'idle');
+    clock.classList.add('rodando');
+}
+
+function setPausedStyle(){
+    clock.classList.remove('rodando', 'idle');
+    clock.classList.add('pausado');
+}
+
+function setIdleStyle(){
+    clock.classList.remove('rodando', 'pausado');
+    clock.classList.add('idle');
+}
+
+init.addEventListener('click', (event) => {
+    clockInit();
+    setRunningStyle();
+})
+
+pause.addEventListener('click', (event) => {
+   clearInterval(timer);
+   timer = null;
+   setPausedStyle();
+})
+
+zero.addEventListener('click', (event) => {
+   clearInterval(timer);
+   seconds = 0;
+   clock.innerHTML = getHourOfSeconds(seconds);
+   setIdleStyle();
 })
