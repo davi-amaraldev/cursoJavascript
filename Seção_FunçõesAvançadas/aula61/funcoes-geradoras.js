d95aca1693b1760564229e9e6b233146311672c1
Davi Amaral <imyuno2022@outlook.com>
2026-04-23 00:19:00 -0300

    Fim da Seção Funções Avançadas)

---
+++ Seção_FunçõesAvançadas/aula61/funcoes-geradoras.js
@@ -0,0 +1,17 @@
+function* geradora(){
+    yield 0;
+    yield 1;
+    yield 2;
+}
+
+function* geradora2(){
+    yield* geradora();
+    yield 3;
+    yield 4;
+    yield 5;
+}
+
+const g4 = geradora2();
+for (let valor of g4){
+    console.log(valor);
+}
